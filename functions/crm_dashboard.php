d20084ea1f502a40e0147914e0d3d840dc1b9449
Adil Soomro <adilsoomro.s@gmail.com>
2023-01-31 04:17:51 +0500

    CRM Functions - enquiry details, enquiry matching listing, enquiry notes added. send email for matching listing CRM Functions - lead enquiries, lead visited listing, lead saved searches, lead notes.

--- functions/crm_dashboard.php
+++ functions/crm_dashboard.php
@@ -16,10 +16,15 @@ add_action( 'litespeed_init', function() {
   $exclude_url_list = array(
       "activities",
       "leads",
+      "lead-details",
+      "lead-saved-searches",
+      "lead-listing-viewed",
+      "lead-notes",
       "delete-lead",
       "enquiries",
       "all-enquiries",
-      "lead-details",
+      "enquiry-matched-listing",
+      "enquiry-notes",
       "deals",
       "delete-deal",
       "delete-crm-enquiry",
@@ -63,6 +68,26 @@ add_action( 'rest_api_init', function () {
       'callback' => 'deleteLead',
     ));
 
+    register_rest_route( 'houzez-mobile-api/v1', '/lead-details', array(
+      'methods' => 'GET',
+      'callback' => 'leadDetails',
+    ));
+
+    register_rest_route( 'houzez-mobile-api/v1', '/lead-listing-viewed', array(
+      'methods' => 'GET',
+      'callback' => 'leadListingViewed',
+    ));
+
+    register_rest_route( 'houzez-mobile-api/v1', '/lead-saved-searches', array(
+      'methods' => 'GET',
+      'callback' => 'leadSavedSearches',
+    ));
+
+    register_rest_route( 'houzez-mobile-api/v1', '/lead-notes', array(
+      'methods' => 'GET',
+      'callback' => 'leadNotes',
+    ));
+
     register_rest_route( 'houzez-mobile-api/v1', '/enquiries', array(
       'methods' => 'GET',
       'callback' => 'getEnquiries',
@@ -73,9 +98,20 @@ add_action( 'rest_api_init', function () {
       'callback' => 'allEnquiries',
     ));
 
-    register_rest_route( 'houzez-mobile-api/v1', '/lead-details', array(
+    register_rest_route( 'houzez-mobile-api/v1', '/enquiry-matched-listing', array(
       'methods' => 'GET',
-      'callback' => 'leadDetails',
+      'callback' => 'enquiryMatchedListing',
+    ));
+
+    register_rest_route( 'houzez-mobile-api/v1', '/send-matched-listing-email', array(
+      'methods' => 'POST',
+      'callback' => 'sendMatchedListingEmail',
+    ));
+
+    
+    register_rest_route( 'houzez-mobile-api/v1', '/enquiry-notes', array(
+      'methods' => 'GET',
+      'callback' => 'enquiryNotes',
     ));
 
     register_rest_route( 'houzez-mobile-api/v1', '/deals', array(
@@ -108,31 +144,84 @@ add_action( 'rest_api_init', function () {
       'callback' => 'deleteCRMEnquiry',
     ));
     
+    register_rest_route( 'houzez-mobile-api/v1', '/add-note', array(
+      'methods' => 'POST',
+      'callback' => 'addNote',
+    ));
+    register_rest_route( 'houzez-mobile-api/v1', '/delete-note', array(
+      'methods' => 'POST',
+      'callback' => 'deleteNote',
+    ));
     
   });
 
-  function doFakeLogin() {
-    if( !isset( $_GET['user_id']) && !isset( $_POST['user_id']) ) {
-      $ajax_response = array( 'success' => false, 'reason' => 'Please provide user_id' );
-      wp_send_json($ajax_response, 400);
-      return;
-    }
-    $user_id  = $_GET['user_id'];
-    if ( empty($user_id) ) {
-      $user_id  = $_POST['user_id'];
-    }
-    if ( empty($user_id) ) {
-      $ajax_response = array( 'success' => false, 'reason' => 'Please provide user_id in POST or GET' );
-      wp_send_json($ajax_response, 400);
-      return;
-    }
+  function allActivities(){
+    //disable lightspeed caching
+      do_action( 'litespeed_control_set_nocache', 'nocache due to logged in' );
+      if (! is_user_logged_in() ) {
+        $ajax_response = array( 'success' => false, 'reason' => 'Please provide user auth.' );
+        wp_send_json($ajax_response, 403);
+        return; 
+      }
+      //a fix for pagination
+      if(isset($_GET["per_page"]) && !empty($_GET["per_page"])) {
+        $_GET["records"] = $_GET["per_page"];
+      }
   
-    $user = get_user_by( 'id', $user_id ); 
-    if( $user ) {
-        wp_set_current_user( $user_id, $user->user_login );
-        wp_set_auth_cookie( $user_id );
-        do_action( 'wp_login', $user->user_login, $user );
-    }
+      $activities = Houzez_Activities::get_activities();
+      $leads_count = Houzez_Leads::get_leads_stats();
+      $resultsold = $activities["data"]["results"];
+      $results = array();
+  
+      
+      
+      foreach( $activities['data']['results'] as $activity ) {
+        $meta = maybe_unserialize($activity->meta);
+        $type = isset($meta['type']) ? $meta['type'] : '';
+        $subtype = isset($meta['subtype']) ? $meta['subtype'] : '';
+  
+        if($type == 'lead') {
+          $permalink_id = isset($meta['listing_id']) ? $meta['listing_id'] : '';
+          if(!empty($permalink_id)) {
+            $meta['title'] = get_the_title($permalink_id);
+            
+          }
+        } else if($type == 'lead_agent') {
+    
+          $permalink_id = isset($meta['agent_id']) ? $meta['agent_id'] : '';
+          $agent_type = isset($meta['agent_type']) ? $meta['agent_type'] : '';
+    
+          if(!empty($permalink_id)) {     
+            if($agent_type == "author_info") {
+              $meta['title'] = get_the_author_meta( 'display_name', $permalink_id );
+            } else {
+              $meta['title'] = get_the_title($permalink_id);
+            }
+          }
+        } else if($type == 'lead_contact') {
+    
+          $permalink_id = isset($meta['lead_page_id']) ? $meta['lead_page_id'] : '';
+          
+          if(!empty($permalink_id)) {
+            $meta['title'] = get_the_title($permalink_id);
+          }
+        }
+        
+        $activity->meta = $meta;
+        array_push($results, $activity);
+      }
+      $activities["data"]["results"] = $results;
+  
+      $deals = array(
+        'active_count' => Houzez_Deals::get_total_deals_by_group('active'),
+        'won_count' => Houzez_Deals::get_total_deals_by_group('won'),
+        'lost_count' => Houzez_Deals::get_total_deals_by_group('lost'),
+      );
+      $activities["data"]["deals"] = $deals;
+  
+      $activities["data"]["stats"] = $leads_count['leads_count'];
+      
+      wp_send_json($activities["data"],200);
   }
 function allLeads() {
   do_action( 'litespeed_control_set_nocache', 'nocache due to logged in' );
@@ -149,74 +238,91 @@ function allLeads() {
   $all_leads = Houzez_leads::get_leads();
   wp_send_json($all_leads["data"],200);
 }
-function allActivities(){
+
+function leadDetails() {
   //disable lightspeed caching
-    do_action( 'litespeed_control_set_nocache', 'nocache due to logged in' );
-    if (! is_user_logged_in() ) {
-      $ajax_response = array( 'success' => false, 'reason' => 'Please provide user auth.' );
-      wp_send_json($ajax_response, 403);
-      return; 
-    }
-    //a fix for pagination
-    if(isset($_GET["per_page"]) && !empty($_GET["per_page"])) {
-      $_GET["records"] = $_GET["per_page"];
-    }
-
-    $activities = Houzez_Activities::get_activities();
-    $leads_count = Houzez_Leads::get_leads_stats();
-    $resultsold = $activities["data"]["results"];
-    $results = array();
-
-    
-    
-    foreach( $activities['data']['results'] as $activity ) {
-      $meta = maybe_unserialize($activity->meta);
-      $type = isset($meta['type']) ? $meta['type'] : '';
-      $subtype = isset($meta['subtype']) ? $meta['subtype'] : '';
-
-      if($type == 'lead') {
-        $permalink_id = isset($meta['listing_id']) ? $meta['listing_id'] : '';
-        if(!empty($permalink_id)) {
-          $meta['title'] = get_the_title($permalink_id);
-          
-        }
-      } else if($type == 'lead_agent') {
+  do_action( 'litespeed_control_set_nocache', 'nocache due to logged in' );
+  if (! is_user_logged_in() ) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide user auth.' );
+    wp_send_json($ajax_response, 403);
+    return; 
+  }
+  //lead-id must be provided
+  if(!isset($_GET["lead-id"]) || empty($_GET["lead-id"])) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide lead-id.' );
+    wp_send_json($ajax_response, 400);
+    return; 
+  }
   
-        $permalink_id = isset($meta['agent_id']) ? $meta['agent_id'] : '';
-        $agent_type = isset($meta['agent_type']) ? $meta['agent_type'] : '';
-  
-        if(!empty($permalink_id)) {     
-          if($agent_type == "author_info") {
-            $meta['title'] = get_the_author_meta( 'display_name', $permalink_id );
-          } else {
-            $meta['title'] = get_the_title($permalink_id);
-          }
-        }
-      } else if($type == 'lead_contact') {
-  
-        $permalink_id = isset($meta['lead_page_id']) ? $meta['lead_page_id'] : '';
-        
-        if(!empty($permalink_id)) {
-          $meta['title'] = get_the_title($permalink_id);
-        }
-      }
-      
-      $activity->meta = $meta;
-      array_push($results, $activity);
-    }
-    $activities["data"]["results"] = $results;
 
-    $deals = array(
-      'active_count' => Houzez_Deals::get_total_deals_by_group('active'),
-      'won_count' => Houzez_Deals::get_total_deals_by_group('won'),
-      'lost_count' => Houzez_Deals::get_total_deals_by_group('lost'),
-    );
-    $activities["data"]["deals"] = $deals;
-
-    $activities["data"]["stats"] = $leads_count['leads_count'];
+  $lead = Houzez_Leads::get_lead($_GET["lead-id"]);
     
-    wp_send_json($activities["data"],200);
+  if (!empty($lead)) {
+    $all_enquires = Houzez_Enquiry::get_enquires();
+    $lead->enquiries = $all_enquires;
+  }
+
+  wp_send_json($lead,200);
+
 }
+
+function leadListingViewed() {
+  //disable lightspeed caching
+  do_action( 'litespeed_control_set_nocache', 'nocache due to logged in' );
+  if (! is_user_logged_in() ) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide user auth.' );
+    wp_send_json($ajax_response, 403);
+    return; 
+  }
+  //lead-id must be provided
+  if(!isset($_GET["lead-id"]) || empty($_GET["lead-id"])) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide lead-id.' );
+    wp_send_json($ajax_response, 400);
+    return; 
+  }
+  $viewed = Houzez_Leads::get_lead_viewed_listings();
+  wp_send_json($viewed,200);
+
+}
+
+function leadSavedSearches() {
+  //disable lightspeed caching
+  do_action( 'litespeed_control_set_nocache', 'nocache due to logged in' );
+  if (! is_user_logged_in() ) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide user auth.' );
+    wp_send_json($ajax_response, 403);
+    return; 
+  }
+  //lead-id must be provided
+  if(!isset($_GET["lead-id"]) || empty($_GET["lead-id"])) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide lead-id.' );
+    wp_send_json($ajax_response, 400);
+    return; 
+  }
+  $searches = Houzez_Leads::get_lead_saved_searches();
+  wp_send_json($searches,200);
+
+}
+function leadNotes() {
+  //disable lightspeed caching
+  do_action( 'litespeed_control_set_nocache', 'nocache due to logged in' );
+  if (! is_user_logged_in() ) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide user auth.' );
+    wp_send_json($ajax_response, 403);
+    return; 
+  }
+  //lead-id must be provided
+  if(!isset($_GET["lead-id"]) || empty($_GET["lead-id"])) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide lead-id.' );
+    wp_send_json($ajax_response, 400);
+    return; 
+  }
+  $belong_to = isset($_GET['lead-id']) ? $_GET['lead-id'] : '';
+  $notes = Houzez_CRM_Notes::get_notes($belong_to, 'lead');
+  wp_send_json($notes,200);
+
+}
+
 
 function getEnquiries(){
   //disable lightspeed caching
@@ -280,7 +386,7 @@ function allEnquiries(){
   
   wp_send_json($all_enquires["data"],200);
 }
-function leadDetails() {
+function enquiryMatchedListing() {
   //disable lightspeed caching
   do_action( 'litespeed_control_set_nocache', 'nocache due to logged in' );
   if (! is_user_logged_in() ) {
@@ -289,21 +395,21 @@ function leadDetails() {
     return; 
   }
   //lead-id must be provided
-  if(!isset($_GET["lead-id"]) || empty($_GET["lead-id"])) {
-    $ajax_response = array( 'success' => false, 'reason' => 'Please provide lead-id.' );
+  if(!isset($_GET["enquiry-id"]) || empty($_GET["enquiry-id"])) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide enquiry-id.' );
     wp_send_json($ajax_response, 400);
     return; 
   }
-  //need only one.
-  $_GET["records"] = "1";
   
-  $all_enquires = Houzez_Enquiry::get_enquires();
+  $enquiry_id =  $_GET["enquiry-id"];
+  
+  $enquiry = Houzez_Enquiry::get_enquiry($enquiry_id);
 
-  $lpage = $_GET["lpage"] ? $_GET["lpage"] : "1";
-  set_query_var('paged', $lpage);
+  $prop_page = $_GET["prop_page"] ? $_GET["prop_page"] : "1";
+  set_query_var('paged', $prop_page);
 
-  $results = array();
-  foreach( $all_enquires['data']['results'] as $enquiry ) {
+  
+  if( !empty($enquiry) ) {
     $meta = maybe_unserialize($enquiry->enquiry_meta);
     $lead = Houzez_Leads::get_lead($enquiry->lead_id);
     $matched_query = matched_listings($enquiry->enquiry_meta);
@@ -314,14 +420,6 @@ function leadDetails() {
         $post_id = $property->ID;
         $property_meta = get_post_meta($post_id);
         $property->property_meta = $property_meta;
-        // $prop_id = houzez_get_listing_data('property_id');
-        // $property->fave_property_size = houzez_get_listing_data('property_size');
-        // $property->fave_property_bathrooms = houzez_get_listing_data('property_bathrooms');
-        // $property->fave_property_bedrooms = houzez_get_listing_data('property_bedrooms');
-        // $property->property_type = houzez_taxonomy_simple('property_type');
-        // $property->fave_property_price = houzez_property_price_crm();
-        // $property->fave_property_id = houzez_propperty_id_prefix($prop_id);
-        // $property->link = get_permalink(get_the_ID());
       endwhile;
     endif;
 
@@ -330,12 +428,52 @@ function leadDetails() {
     $enquiry->lead = $lead;
     $enquiry->matched = $matched_query->posts;
 
-    array_push($results, $enquiry);
+    
   }
-  $all_enquires["data"]["results"] = $results;
   
-  wp_send_json($all_enquires["data"],200);
+  
+  wp_send_json($enquiry,200);
 
+}
+function enquiryNotes() {
+  //disable lightspeed caching
+  do_action( 'litespeed_control_set_nocache', 'nocache due to logged in' );
+  if (! is_user_logged_in() ) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide user auth.' );
+    wp_send_json($ajax_response, 403);
+    return; 
+  }
+  //lead-id must be provided
+  if(!isset($_GET["enquiry-id"]) || empty($_GET["enquiry-id"])) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide enquiry-id.' );
+    wp_send_json($ajax_response, 400);
+    return; 
+  }
+  $belong_to = isset($_GET['enquiry-id']) ? $_GET['enquiry-id'] : '';
+  $notes = Houzez_CRM_Notes::get_notes($belong_to, 'enquiry');
+  wp_send_json($notes,200);
+
+}
+function sendMatchedListingEmail() {
+  if (! is_user_logged_in() ) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide user auth.' );
+    wp_send_json($ajax_response, 403);
+    return; 
+  }
+  
+  if(!isset($_POST["ids"]) || empty($_POST["ids"])) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide selected property ids.' );
+    wp_send_json($ajax_response, 400);
+    return; 
+  }
+  if(!isset($_POST["email_to"]) || empty($_POST["email_to"])) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide email_to.' );
+    wp_send_json($ajax_response, 400);
+    return; 
+  }
+  
+  do_action("wp_ajax_houzez_match_listing_email");
+  
 }
 function allDeals() {
   //disable lightspeed caching
@@ -446,3 +584,46 @@ function deleteCRMEnquiry() {
   //needs enquiry id in var ids
   do_action("wp_ajax_houzez_delete_enquiry");
 }
+
+function addNote() {
+  if (! is_user_logged_in() ) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide user auth.' );
+    wp_send_json($ajax_response, 403);
+    return; 
+  }
+  $nonce = wp_create_nonce('note_add_nonce');
+  $_REQUEST['security'] = $nonce;
+  if(!isset($_POST["note"]) || empty($_POST["note"])) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide note.' );
+    wp_send_json($ajax_response, 400);
+    return; 
+  }
+  if(!isset($_POST["note_type"]) || empty($_POST["note_type"])) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide note type.' );
+    wp_send_json($ajax_response, 400);
+    return; 
+  }
+  if(!isset($_POST["belong_to"]) || empty($_POST["belong_to"])) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide note parent.' );
+    wp_send_json($ajax_response, 400);
+    return; 
+  }
+  do_action("wp_ajax_houzez_crm_add_note");
+  
+}
+function deleteNote() {
+
+  if (! is_user_logged_in() ) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide user auth.' );
+    wp_send_json($ajax_response, 403);
+    return; 
+  }
+  if(!isset($_POST["note_id"]) || empty($_POST["note_id"])) {
+    $ajax_response = array( 'success' => false, 'reason' => 'Please provide note parent.' );
+    wp_send_json($ajax_response, 400);
+    return; 
+  }
+  //needs enquiry id in var ids
+  do_action("wp_ajax_houzez_delete_note");
+}
+
